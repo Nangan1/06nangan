6594ef4acec7b27cf811d7fdd83b7a42ca263828
휘현 김 <zwnmu@naver.com>
2021-07-22 10:47:56 +0900

    초기 커밋.

---
+++ NangAn/NangAn.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a[3];
+	int b;
+
+	do {
+		cin >> a[0];
+		cin >> a[1];
+		cin >> a[2];
+
+		switch (a[1]) {
+		case '+': 
+			b = (a[0] - 48) + (a[2] - 48);
+			cout << b;
+			break;
+		case '-':
+			b = (a[0] - 48) - (a[2] - 48);
+			cout << b;
+			break;
+		case '*':
+			b = (a[0] - 48) * (a[2] - 48);
+			cout << b;
+			break;
+		case '/':
+			b = (a[0] - 48) / (a[2] - 48);
+			cout << b;
+			break;
+		default:
+			return 0;
+		}
+	} while ('e' != a[0] && 'e' != a[1] && 'e' != a[2]);
+	return 0;
+}
